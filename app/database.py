fa7ef9171e18a287385c0aca6107839fc0c1dc83
Precious Ohanyere <obbyprecious24@gmail.com>
2024-01-01 23:08:12 +0100

    delete posts created by a deleted user account

--- app/database.py
+++ app/database.py
@@ -1,8 +1,10 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv, find_dotenv
-import os
+
 
 load_dotenv(find_dotenv())
 SQLALCHEMY_DATABASE_URL = os.environ.get("SQLALCHEMY_DATABASE_URL")
